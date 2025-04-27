1bff8ad8b98540bac39287f6f78d8be1a5a083a7
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2025-04-27 19:45:46 +0200

    change sorting

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -22,7 +22,7 @@ class ContactResource extends Resource
 
     protected static ?string $navigationGroup = 'Customer Interactions';
 
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 9;
 
     public static function form(Form $form): Form
     {
